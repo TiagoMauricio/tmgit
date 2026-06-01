dbfa29f99b17133cb2b3c68693b2e8718ff0e736
Tiago <tiagolcmauricio@gmail.com>
2026-06-01 18:57:49 +0100

    Add git operatioins and DRY implementation

--- helpers/git_operations.py
+++ helpers/git_operations.py
@@ -1,15 +1,37 @@
 import subprocess
 from pathlib import Path
+from turtle import resetscreen
+
 import typer
 
-def sync_current():
-    cwd_git_file = Path.cwd()/".git"
-    if not cwd_git_file.exists():
-        print("This is not a git repo. Abort...")
-        exit(1)
-    result = subprocess.run(["git","pull", "origin", "HEAD" ], capture_output=True, text=True)
+
+def _execute(*args):
+    result = subprocess.run(*args)
     if result.returncode == 0:
         typer.echo(result.stdout)
     else:
         typer.echo(result.stderr)
 
+
+def _check_if_git():
+    cwd_git_file = Path.cwd() / ".git"
+    if not cwd_git_file.exists():
+        print("This is not a git repo. Abort...")
+        exit(1)
+
+
+def _fetch_current():
+    return "main"
+
+
+def sync_current():
+    _check_if_git()
+    return _execute("git", "pull", "origin", _fetch_current())
+
+
+def sync_workspace():
+    print("this is sync workspace")
+
+
+def sync_repo():
+    print("this is sync repo")
--- main.py
+++ main.py
@@ -1,55 +1,73 @@
-import json, os, time, typer, subprocess
-import helpers.constants as constants, helpers.git_operations as git_operations
-from pathlib import Path
-from platformdirs import PlatformDirs
-from rich.progress import track
-from typing import Annotated
+import json
 from enum import Enum
+from pathlib import Path
+from typing import Annotated
 
+import typer
+from platformdirs import PlatformDirs
+
+import helpers.constants as constants
+import helpers.git_operations as git_operations
 
 CONFIG_FILE_NAME = "config.json"
 CONFIG_DIR = Path(PlatformDirs("tmgit").user_config_dir)
 CONFIG_FILE = CONFIG_DIR / CONFIG_FILE_NAME
 DEFAULT_GIT_DIR = Path.home() / "git"
 
-#print(DEFAULT_GIT_DIR)
+# print(DEFAULT_GIT_DIR)
 
 CONFIG_DIR.mkdir(parents=True, exist_ok=True)
 
 # App inits
 app = typer.Typer(no_args_is_help=True)
 
+
 # Enums
 class SyncTarget(str, Enum):
     repo = "repo"
-    ws   = "ws"
+    workspace = "ws"
     current = "current"
+
+
+TARGET_FUNCTIONS = {
+    SyncTarget.current: git_operations.sync_current,
+    SyncTarget.workspace: git_operations.sync_workspace,
+    SyncTarget.repo: git_operations.sync_repo,
+}
+
 
 # Functions
 def _init_config():
-    return { "git_ws_dir": str(DEFAULT_GIT_DIR) }
+    return {"git_ws_dir": str(DEFAULT_GIT_DIR)}
+
 
 def _load_config():
     if CONFIG_FILE.exists():
-        #print(f"Loading current config file: {CONFIG_FILE}")
+        # print(f"Loading current config file: {CONFIG_FILE}")
         return json.loads(CONFIG_FILE.read_text())
 
-        #print(f"Config file does not exist. Creating config at: {CONFIG_FILE}")
+        # print(f"Config file does not exist. Creating config at: {CONFIG_FILE}")
     new_config = _init_config()
     _save_config(new_config)
     return new_config
 
+
 def _save_config(config: dict):
     CONFIG_FILE.write_text(json.dumps(config, indent=2))
+
 
 @app.command()
 def config():
     print("Running config")
 
+
 @app.command("sync")
-def sync(target: Annotated[SyncTarget, typer.Argument(help=constants.HELP_SYNC_TARGET)]):
-    if target == "current":
-        return git_operations.sync_current()
+def sync(
+    target: Annotated[SyncTarget, typer.Argument(help=constants.HELP_SYNC_TARGET)],
+):
+    return TARGET_FUNCTIONS[target]()
+
+
 if __name__ == "__main__":
     _load_config()
     app()
