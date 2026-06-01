eae66d2d8a7b6544cb0287812a6ac71617b79590
Tiago <tiagolcmauricio@gmail.com>
2026-06-01 23:25:33 +0100

    Add some color

--- helpers/git_operations.py
+++ helpers/git_operations.py
@@ -1,32 +1,42 @@
 import subprocess
 from pathlib import Path
 from turtle import resetscreen
+from rich import print
 
 import typer
 
+#TODO: add a function to handle output
 
-def _execute(*args):
-    result = subprocess.run(*args)
+def _execute(command_string: str):
+    print(f"[green]Executing:[/green] {command_string}")
+    result = subprocess.run(command_string.split(), capture_output=True, text=True)
     if result.returncode == 0:
-        typer.echo(result.stdout)
+        print(result.stdout)
+        return(result)
     else:
-        typer.echo(result.stderr)
+        print(f"[red]{result.stderr}[/red]")
+        exit(1)
 
 
 def _check_if_git():
     cwd_git_file = Path.cwd() / ".git"
     if not cwd_git_file.exists():
-        print("This is not a git repo. Abort...")
+        print("[red]This is not a git repo. Abort...[/red]")
         exit(1)
 
 
 def _fetch_current():
-    return "main"
-
+    result = subprocess.run("git branch --show-current".split(), capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout.rstrip()
+    else:
+        print(f"[red]Failed to fetch current branch with err: {result.stderr}[/red]")
+        exit(1)
 
 def sync_current():
     _check_if_git()
-    return _execute("git", "pull", "origin", _fetch_current())
+    current_branch = _fetch_current()
+    return _execute(f"git pull origin {current_branch}")
 
 
 def sync_workspace():
